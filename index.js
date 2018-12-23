653c0cbd947d579d3c51e6530e99e345bb7d8137
DipanshKhandelwal <deepanshrocks438@gmail.com>
2018-12-23 06:14:16 +0530

    Added subscribe to listen to changes of a state

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function createStore () {
+
     let state
+    let listener = []
 
     const getState = () => state
 
+    const subscribe = (listener) => {
+        listener.push(listener)
+        return () => {
+            listeners = listener.filter((l) => l !== listener)
+        }
+    } 
+
     return {
-        getState
+        getState,
+        subscribe
     }
 }
